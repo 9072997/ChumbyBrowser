ab0e39c3c010cda6dab7cda20b12a90d6002f8b4
Jon Penn <pennpd@sbcglobal.net>
2013-03-25 21:08:57 -0500

    added source for js keybord which is not human readible in builds

---
+++ userapp/www/share/keyboard/js/keyboard.js
@@ -0,0 +1,61 @@
+$(function(){
+	var $write = $('#write'),
+		shift = false,
+		capslock = false;
+	
+	$('#keyboard li').click(function(){
+		var $this = $(this),
+			character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
+		
+		// Shift key
+		if ($this.hasClass('left-shift')) {
+			$('.letter').toggleClass('uppercase');
+			$('.symbol span').toggle();
+			
+			shift = (shift === true) ? false : true;
+			capslock = false;
+			return false;
+		}
+		
+		// done key
+		if ($this.hasClass('done')) {
+			document.getElementsByTagName('form')[0].submit();
+			return false;
+		}
+		
+		// Caps lock
+		if ($this.hasClass('capslock')) {
+			$('.letter').toggleClass('uppercase');
+			capslock = true;
+			return false;
+		}
+		
+		// Delete
+		if ($this.hasClass('delete')) {
+			var html = $write.html();
+			
+			$write.html(html.substr(0, html.length - 1));
+			return false;
+		}
+		
+		// Special characters
+		if ($this.hasClass('symbol')) character = $('span:visible', $this).html();
+		if ($this.hasClass('space')) character = ' ';
+		if ($this.hasClass('tab')) character = "\t";
+		if ($this.hasClass('return')) character = "\n";
+		
+		// Uppercase letter
+		if ($this.hasClass('uppercase')) character = character.toUpperCase();
+		
+		// Remove shift once a key is clicked.
+		if (shift === true) {
+			$('.symbol span').toggle();
+			if (capslock === false) $('.letter').toggleClass('uppercase');
+			
+			shift = false;
+		}
+		
+		// Add the character
+		$write.html($write.html() + character);
+	});
+});
